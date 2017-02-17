dd2485d6c3061a3b1009157daab315d96b1657be
Eric McCormick <Eric McCormick>
2017-02-17 16:41:47 -0500

    finished initial draft pulling thumbnails

---
+++ poorMan.js
@@ -0,0 +1,61 @@
+//start of js
+$(function() {
+
+  var url = 'https://www.reddit.com/r/aww/.json'
+
+$.get(url).done(function(responseBody){
+  //simple test
+  console.log("DONE", responseBody.data.children[1].data.title);
+  //actuall jazz to post on the page
+  //foreach loop for dispaly
+  responseBody.data.children.forEach(function(post){
+    //making variables
+  var t =post.data.title;
+  var a =post.data.author;
+  var d =post.data.created_utc;
+        var realTime = new Date(d*1000);
+  var p =post.data.thumbnail;
+  //why is this one an issue?
+  //var i =post.data.preview.images.resolutions.url;
+  //from prior assignment
+  var $header = $('<h3>').text(t);
+  var $paragraph = $('<p>').addClass("details");
+  var $author = $('<span>').addClass("author").text("Author: "+a);
+  var $date = $('<span>').addClass("date").text("UTC Time Created: "+realTime);
+  var $thumbnail = $('<div>').addClass("picture").html("<img src="+p+">"+"</img>");
+  //var $image = $('<div>').addClass("fullPicture").html("<img src="+i+">"+"</img>");
+
+  //variable to post
+
+
+//posting to page
+$('body').append($header)
+      .append($paragraph.append($author).append($date))
+      .append($thumbnail)
+      //.append($image)
+
+//end of ammending
+;
+
+
+});
+
+
+}).fail(function() {
+  console.log("FAIL");
+}).always(function() {
+  console.log("ALWAYS");
+});
+
+
+
+
+
+
+
+
+  //end of ajax
+
+
+//end of JS
+});
