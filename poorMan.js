b31f0f1db9e3b90be0e9983d1149d880ff67bd38
Andrea Haupt <hauptaj@gmail.com>
2017-02-19 13:34:31 -0500

    changed the order of the headline and author info

--- poorMan.js
+++ poorMan.js
@@ -14,45 +14,31 @@ $(function() {
             var a = post.data.author;
             var d = post.data.created_utc;
             // CONVERT TIMESTAMP TO READABLE DATE
-            var date = post.data.created_utc;
-            var timestampInMilliSeconds = date * 1000;
-            var date = new Date(timestampInMilliSeconds);
-            var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
-            var month = (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1);
-            var year = date.getFullYear();
-            var hours = ((date.getHours() % 12 || 12) < 10 ? '0' : '') + (date.getHours() % 12 || 12);
-            var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-            var meridiem = (date.getHours() >= 12) ? 'pm' : 'am';
-            var formattedDate = month + '-' + day + '-' + year + ' at ' + hours + ':' + minutes + ' ' + meridiem;
-
-
+                var date = post.data.created_utc;
+                var timestampInMilliSeconds = date * 1000;
+                var date = new Date(timestampInMilliSeconds);
+                var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+                var month = (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1);
+                var year = date.getFullYear();
+                var hours = ((date.getHours() % 12 || 12) < 10 ? '0' : '') + (date.getHours() % 12 || 12);
+                var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+                var meridiem = (date.getHours() >= 12) ? 'pm' : 'am';
+                var formattedDate = month + '-' + day + '-' + year + ' at ' + hours + ':' + minutes + ' ' + meridiem;
             var p = post.data.thumbnail;
             //why is this one an issue?
             //var imgs =post.data.preview.images.source.url;
             //from prior assignment
 
-            var $header = $('<h3>').text(t);
-            var $paragraph = $('<p>').addClass("details");
-            var $author = $('<span>').addClass("author").text("Created by: " + a + " on " + formattedDate);
-            // var $date = $('<span>').addClass("date").text(" Created: "+formattedDate);
+            var $header = $('<div>').addClass('info').html('<span class="headline">' + t + '</span>' + '<span class="author">' + "Posted by: " + a + " on " + formattedDate + "</span>");
             var $thumbnail = $('<div>').addClass("picture").html("<img src=" + p + ">" + "</img>");
-            //var $image = $('<div>').addClass("fullPicture").html("<img src="+imgs+">"+"</img>");
-
-            //variable to post
 
 
             //posting to page
             $('.postContainer')
                 .append($thumbnail)
-                .append($author)
                 .append($header)
-                // .append($author)
-                .append($paragraph)
-            // .append($author)
-            // .append($paragraph)
-            // .append($date)
-            // .append($thumbnail)
-            //.append($image)
+
+
 
             //end of ammending
             ;
