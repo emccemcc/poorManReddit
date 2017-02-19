986fa0da02c45ff1ee8deba425b100ecc2dcc054
Andrea Haupt <hauptaj@gmail.com>
2017-02-19 08:52:51 -0500

    worked on alignment

--- poorMan.js
+++ poorMan.js
@@ -1,24 +1,19 @@
 //start of js
 $(function() {
 
-  var url = 'https://www.reddit.com/r/aww/.json'
+    var url = 'https://www.reddit.com/r/aww/.json'
 
-$.get(url).done(function//start of js
-$(function() {
-
-  var url = 'https://www.reddit.com/r/aww/.json'
-
-$.get(url).done(function(responseBody){
-  //simple test
-  console.log("DONE", responseBody.data.children[1].data.title);
-  //actuall jazz to post on the page
-  //foreach loop for dispaly
-  responseBody.data.children.forEach(function(post){
-    //making variables
-  var t =post.data.title;
-  var a =post.data.author;
-  var d =post.data.created_utc;
-  // CONVERT TIMESTAMP TO READABLE DATE
+    $.get(url).done(function(responseBody) {
+        //simple test
+        console.log("DONE", responseBody.data.children[1].data.title);
+        //actuall jazz to post on the page
+        //foreach loop for dispaly
+        responseBody.data.children.forEach(function(post) {
+            //making variables
+            var t = post.data.title;
+            var a = post.data.author;
+            var d = post.data.created_utc;
+            // CONVERT TIMESTAMP TO READABLE DATE
             var date = post.data.created_utc;
             var timestampInMilliSeconds = date * 1000;
             var date = new Date(timestampInMilliSeconds);
@@ -31,95 +26,46 @@ $.get(url).done(function(responseBody){
             var formattedDate = month + '-' + day + '-' + year + ' at ' + hours + ':' + minutes + ' ' + meridiem;
 
 
-  var p =post.data.thumbnail;
-  //why is this one an issue?
-  //var imgs =post.data.preview.images.source.url;
-  //from prior assignment
-  var $header = $('<h3>').text(t);
-  var $paragraph = $('<p>').addClass("details");
-  var $author = $('<span>').addClass("author").text("Created by: "+a + " on " + formattedDate);
-  // var $date = $('<span>').addClass("date").text(" Created: "+formattedDate);
-  var $thumbnail = $('<div>').addClass("picture").html("<img src="+p+">"+"</img>");
-  //var $image = $('<div>').addClass("fullPicture").html("<img src="+imgs+">"+"</img>");
+            var p = post.data.thumbnail;
+            //why is this one an issue?
+            //var imgs =post.data.preview.images.source.url;
+            //from prior assignment
 
-  //variable to post
+            var $header = $('<h3>').text(t);
+            var $paragraph = $('<p>').addClass("details");
+            var $author = $('<span>').addClass("author").text("Created by: " + a + " on " + formattedDate);
+            // var $date = $('<span>').addClass("date").text(" Created: "+formattedDate);
+            var $thumbnail = $('<div>').addClass("picture").html("<img src=" + p + ">" + "</img>");
+            //var $image = $('<div>').addClass("fullPicture").html("<img src="+imgs+">"+"</img>");
 
-
-//posting to page
-$('.postContainer').append($header)
-      .append($paragraph).append($author)
-      // .append($date)
-      .append($thumbnail)
-      //.append($image)
-
-//end of ammending
-;
+            //variable to post
 
 
-});
+            //posting to page
+            $('.postContainer')
+                .append($thumbnail)
+                .append($author)
+                .append($header)
+                // .append($author)
+                .append($paragraph)
+            // .append($author)
+            // .append($paragraph)
+            // .append($date)
+            // .append($thumbnail)
+            //.append($image)
+
+            //end of ammending
+            ;
 
 
-}).fail(function() {
-  console.log("FAIL");
-}).always(function() {
-  console.log("ALWAYS");
-});
+        });
 
 
-
-
-
-
-
-
-  //end of ajax
-
-
-//end of JS
-});
-(responseBody){
-  //simple test
-  console.log("DONE", responseBody.data.children[1].data.title);
-  //actuall jazz to post on the page
-  //foreach loop for dispaly
-  responseBody.data.children.forEach(function(post){
-    //making variables
-  var t =post.data.title;
-  var a =post.data.author;
-  var d =post.data.created_utc;
-        var realTime = new Date(d*1000);
-  var p =post.data.thumbnail;
-  //why is this one an issue?
-  //var imgs =post.data.preview.images.source.url;
-  //from prior assignment
-  var $header = $('<h3>').text(t);
-  var $paragraph = $('<p>').addClass("details");
-  var $author = $('<span>').addClass("author").text("Author: "+a);
-  var $date = $('<span>').addClass("date").text("UTC Time Created: "+realTime);
-  var $thumbnail = $('<div>').addClass("picture").html("<img src="+p+">"+"</img>");
-  //var $image = $('<div>').addClass("fullPicture").html("<img src="+imgs+">"+"</img>");
-
-  //variable to post
-
-
-//posting to page
-$('body').append($header)
-      .append($paragraph.append($author).append($date))
-      .append($thumbnail)
-      //.append($image)
-
-//end of ammending
-;
-
-
-});
-
-
-}).fail(function() {
-  console.log("FAIL");
-}).always(function() {
-  console.log("ALWAYS");
-});
+    }).fail(function() {
+        console.log("FAIL");
+    }).always(function() {
+        console.log("ALWAYS");
+    });
 
 
 
@@ -128,8 +74,8 @@ $('body').append($header)
 
 
 
-  //end of ajax
+    //end of ajax
 
 
-//end of JS
+    //end of JS
 });
